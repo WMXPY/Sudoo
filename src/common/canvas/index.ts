5be78b8cb7263e24325ba4c1bf00bb8aaf6577d5
WMXPY <wm@mengw.io>
2018-09-26 10:09:15 -0500

    fix lines

--- src/common/canvas/index.ts
+++ src/common/canvas/index.ts
@@ -56,13 +56,7 @@ export class Canvas implements ICanvas {
             return this;
         }
 
-        const lastDrawLines: number = this._lastDraw.split('\n').length;
-        if (lastDrawLines > 1) {
-            this.clear(lastDrawLines - 1);
-        } else {
-            this.clear();
-        }
-
+        this.clear(this._replaceLines());
         this.draw(...contents);
         return this;
     }
@@ -77,14 +71,14 @@ export class Canvas implements ICanvas {
         return this;
     }
 
-    public clear(lines?: number): ICanvas {
+    public clear(lines: number = 0): ICanvas {
         this.cursor(0);
-        (this._stream as any).clearLine();
+        // (this._stream as any).clearLine();
 
         if (lines) {
-            for (let i = 0; i < lines; i++) {
+            for (let i = 0; i < lines - 1; i++) {
                 this.cursor(0, true);
-                (this._stream as any).clearLine();
+                // (this._stream as any).clearLine();
             }
         }
 
@@ -96,5 +90,20 @@ export class Canvas implements ICanvas {
         if (info) this.draw(info);
         process.exit(signal);
         return;
+    }
+
+    protected _width(): number {
+        const width: number | undefined = process.stdout.columns;
+        return width || Number.MAX_SAFE_INTEGER;
+    }
+
+    protected _replaceLines(): number {
+        const lastDrawLines: string[] = this._lastDraw.split('\n');
+        const width: number = this._width();
+        const results: number = lastDrawLines.reduce((previous: number, line: string) => {
+            return previous + Math.ceil(line.length / (width));
+        }, 0);
+
+        return results || 0;
     }
 }
